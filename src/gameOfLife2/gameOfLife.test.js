ea5f74506b3160563e4ca62edb3d46a6611075e1
James Logue <logueja@gmail.com>
2018-03-17 11:19:13 -0600

    Solved as GameOfLife2

---
+++ src/gameOfLife2/gameOfLife.test.js
@@ -0,0 +1,11 @@
+import { gameOfLife } from './gameOfLife'
+
+describe('game of life', () => {
+
+  it('it exists', () => {
+    expect(gameOfLife(3).length).toEqual (3)
+  })
+  it('', () => {
+
+  })
+})
